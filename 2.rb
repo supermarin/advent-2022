0308cffa2c407402cd7e69ab24bd30460dd4d532
Marin Usalj <marin2211@gmail.com>
2022-12-04 17:54:51 +0100

    solargraph, simplify reduce

--- 2.rb
+++ 2.rb
@@ -1,62 +1,58 @@
 suits = {
-  "A" => 1, # rock
-  "X" => 1, # rock
-  "B" => 2, # paper
-  "Y" => 2, # paper
-  "C" => 3, # scissor
-  "Z" => 3, # scissor
+  'A' => 1, # rock
+  'X' => 1, # rock
+  'B' => 2, # paper
+  'Y' => 2, # paper
+  'C' => 3, # scissor
+  'Z' => 3 # scissor
 }
 
-losewindraw = { 
-  "A Y" => 6,
-  "A Z" => 0,
-  "B Z" => 6,
-  "B X" => 0,
-  "C X" => 6,
-  "C Y" => 0,
+losewindraw = {
+  'A Y' => 6,
+  'A Z' => 0,
+  'B Z' => 6,
+  'B X' => 0,
+  'C X' => 6,
+  'C Y' => 0,
 
-  "A X" => 3,
-  "B Y" => 3,
-  "C Z" => 3,
+  'A X' => 3,
+  'B Y' => 3,
+  'C Z' => 3
 }
 
 scores_2 = {
-  "X" => 0, # lose
-  "Y" => 3, # draw
-  "Z" => 6, # win
+  'X' => 0, # lose
+  'Y' => 3, # draw
+  'Z' => 6 # win
 }
 
-our_suit = { 
-  "A X" => "C",
-  "A Y" => "A",
-  "A Z" => "B",
-  "B X" => "A",
-  "B Y" => "B",
-  "B Z" => "C",
-  "C X" => "B",
-  "C Y" => "C",
-  "C Z" => "A",
+our_suit = {
+  'A X' => 'C',
+  'A Y' => 'A',
+  'A Z' => 'B',
+  'B X' => 'A',
+  'B Y' => 'B',
+  'B Z' => 'C',
+  'C X' => 'B',
+  'C Y' => 'C',
+  'C Z' => 'A'
 }
 
-input = File.readlines("2-input.txt")
+input = File.readlines('2-input.txt')
 
-i = -1
-games = input.reduce([]) do |acc, g|
-  i += 1
+games = input.reduce(0) do |acc, g|
   round = g.chomp # g = String "C X\n"
-  they, us = round.split(" ")
-  acc[i] = losewindraw[round] + suits[us] 
+  they, us = round.split(' ')
+  acc += losewindraw[round] + suits[us]
   acc
 end
-puts "First: #{games.sum}"
+puts "First: #{games}"
 
-i = -1
-games_2 = input.reduce([]) do |acc, g|
-  i += 1
+games2 = input.reduce(0) do |acc, g|
   round = g.chomp # g = String "C X\n"
-  they, outcome = round.split(" ")
+  they, outcome = round.split(' ')
   us = our_suit[round]
-  acc[i] = scores_2[outcome] + suits[us] 
+  acc += scores_2[outcome] + suits[us]
   acc
 end
-puts "Second: #{games_2.sum}"
+puts "Second: #{games2}"
