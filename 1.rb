6ad14127906db327e42ea34ba59d49f36ca1dacf
Marin Usalj <marin2211@gmail.com>
2022-12-04 17:55:19 +0100

    frozen_string_literal

--- 1.rb
+++ 1.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 input = File.read('1-input.txt').split("\n\n")
 puts "First: #{input.map { |e| e.split("\n").map(&:to_i).sum }.max}"
 puts "Second: #{input.map { |e| e.split("\n").map(&:to_i).sum }.sort[-3..].sum}"
