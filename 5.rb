bdc7cdfb989009dafffc45c76260d11523a250a2
Marin Usalj <marin2211@gmail.com>
2022-12-05 16:26:41 +0100

    Day 5 2nd solution
    There's a problem if both solutions are uncommented:
    - the `stacks` hash gets modified while being in the `reduce`
    - this is because it's not deeply copied and modified it's values
      modifies the original hash values.
    
    TODO: for practice, resolve it so it works when both solutions run at
    the same time.

--- 5.rb
+++ 5.rb
@@ -2,7 +2,7 @@
 
 require 'strscan'
 
-input = File.read('5-input.txt')
+input = File.read('5-input-test.txt')
 cs, moves = input.split("\n\n").map { |l| l.split("\n") }
 crates = cs[0...-1]
 
@@ -12,20 +12,29 @@ revstacks = cs[-1].chars.each_with_object({}).with_index do |(c, a), i|
   a[i] = c if c.to_i != 0
 end
 
-stacks = crates.each_with_object({}) do |row, acc|
+stacks = crates.reduce({}) do |acc, row|
   row.chars.each_with_index do |c, i|
     (acc[revstacks[i]] ||= []).unshift(c) if c =~ /\w/
   end
   acc
 end
 
-moved = moves.each_with_object(stacks) do |move, acc|
+# moved = moves.reduce(stacks.clone) do |acc, move|
+#   scanner = StringScanner.new(move)
+#   scanner.scan(/move (\d+) from (\d) to (\d)/)
+#   howmany, from, to = scanner.captures
+#   taken = acc[from].pop(howmany.to_i)
+#   taken.reverse.map { |e| acc[to] << e }
+#   acc
+# end
+# puts "First: #{moved.keys.sort.map { |k| moved[k].last }.join}"
+
+moved2 = moves.reduce(stacks.clone) do |acc, move|
   scanner = StringScanner.new(move)
   scanner.scan(/move (\d+) from (\d) to (\d)/)
   howmany, from, to = scanner.captures
   taken = acc[from].pop(howmany.to_i)
-  taken.reverse.map { |e| acc[to] << e }
+  taken.map { |e| acc[to] << e }
   acc
 end
-
-puts "First: #{moved.keys.sort.map { |k| moved[k].last }.join}"
+puts "Second: #{moved2.keys.sort.map { |k| moved2[k].last }.join}"
