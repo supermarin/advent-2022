20a251d08dde1f996636639e29f4a645380ccf84
Marin Usalj <marin2211@gmail.com>
2022-12-19 14:45:50 -0500

    day 10 part 2

--- 10.rb
+++ 10.rb
@@ -1,13 +1,33 @@
-require 'pry-byebug'
 input = File.read('10-input.txt').lines.map(&:chomp)
+
+class CRT
+  attr_reader :screen
+
+  def initialize
+    @screen = []
+  end
+
+  def draw(cycle, x)
+    c = cycle - 1
+    row = c / 40
+    col = c % 40
+    @screen[row] ||= []
+    @screen[row][col] = if (x - 1..x + 1).include?(col)
+                          '#'
+                        else
+                          '.'
+                        end
+  end
+end
 
 class CPU
   attr_reader :sum
 
-  def initialize
+  def initialize(crt)
     @x = 1
     @cycle = 0
     @sum = 0
+    @crt = crt
   end
 
   def addx(num)
@@ -28,16 +48,20 @@ class CPU
   def process(instructions)
     until instructions.empty?
       cycle
+      @crt.draw(@cycle, @x)
       instr, arg = instructions.shift.split
 
       next if instr == 'noop'
 
       cycle
+      @crt.draw(@cycle, @x)
       addx(arg) if instr == 'addx'
     end
   end
 end
 
-cpu = CPU.new
+crt = CRT.new
+cpu = CPU.new(crt)
 cpu.process(input)
 puts "First: #{cpu.sum}"
+puts "Second:\n#{crt.screen.map(&:join).join("\n")}"
